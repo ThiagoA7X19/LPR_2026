f55eec0a5b9cce8c863881758ecbb5ced746b232
ThiagoA7X19 <thiagopcosta08@gmail.com.br>
2026-03-17 13:52:00 -0300

    aula_4

---
+++ AULA_4/Exercicio1lpr/exercicioum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace vefnum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número inteiro:");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("O número é par.");
+            }
+            else
+            {
+                Console.WriteLine("O número é ímpar.");
+            }
+        }
+    }
+}
