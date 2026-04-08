f74d935553f54872269fb602aaa25fe0c537f89b
ThiagoA7X19 <thiagopcosta08@gmail.com.br>
2026-04-08 10:48:14 -0300

    aula6

---
+++ AULA_6/exercicio3/exercicio3.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Runtime.CompilerServices;
+class Program
+{
+    static void cadastrarHeroi()
+    {
+        Console.WriteLine("Digite o nomde do herói da Marvel: ");
+        string nome = Console.ReadLine();
+        Console.WriteLine("Digite o poder do herói: ");
+        string poder = Console.ReadLine();
+        Console.WriteLine("Digite a pontuação do herói: ");
+        int pontuacao = int.Parse(Console.ReadLine());
+
+    }
+    static void Main(string[] args)
+    {
+        int quantidadeHerois = 0;
+        while (quantidadeHerois < 5)
+        {
+            Console.WriteLine($"Cadastro do herói {quantidadeHerois + 1}");
+            cadastrarHeroi();
+            quantidadeHerois++;
+        }
+    }
+}   
