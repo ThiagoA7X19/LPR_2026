4bdc2b970b1a8a495f6663f01df597da3b1ff33a
Thiago <thiagopcosta08@gmail.com>
2026-06-07 16:04:00 -0300

    resolvendo exercicio3

--- AULA_7/exercicio3matriz.cpp
+++ AULA_7/exercicio3matriz.cpp
@@ -1,11 +1,5 @@
 #include <iostream>
 using namespace std;
-int matriz[4][4];
-
-int min() {
-std::cout << "Digite o nome de duas cidades: " << std::endl;
-string cidade1, cidade2;
-cin >> cidade1 >> cidade2;
 
 int matriz[4][4] = {
     {0, 524, 521, 882},
@@ -13,3 +7,58 @@ int matriz[4][4] = {
     {521, 434, 0, 429},
     {882, 586, 429, 0}
 };
+
+int calcularDistancia(string cidade1, string cidade2)
+{
+    int indice1, indice2;
+
+    if (cidade1 == "A")
+        indice1 = 0;
+    else if (cidade1 == "B")
+        indice1 = 1;
+    else if (cidade1 == "C")
+        indice1 = 2;
+    else if (cidade1 == "D")
+        indice1 = 3;
+    else
+    {
+        cout << "Cidade inválida!" << endl;
+        return -1;
+    }
+
+    if (cidade2 == "A")
+        indice2 = 0;
+    else if (cidade2 == "B")
+        indice2 = 1;
+    else if (cidade2 == "C")
+        indice2 = 2;
+    else if (cidade2 == "D")
+        indice2 = 3;
+    else
+    {
+        cout << "Cidade inválida!" << endl;
+        return -1;
+    }
+
+    return matriz[indice1][indice2];
+}
+
+int main()
+{
+    string cidade1, cidade2;
+
+    cout << "Digite o nome de duas cidades (A, B, C ou D): ";
+    cin >> cidade1 >> cidade2;
+
+    int distancia = calcularDistancia(cidade1, cidade2);
+
+    if (distancia != -1)
+    {
+        cout << "A distancia entre "
+             << cidade1 << " e "
+             << cidade2 << " é "
+             << distancia << " km." << endl;
+    }
+
+    return 0;
+}
