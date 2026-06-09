bcfe5228ced737661a48af1d34c2bc7df97da613
Thiago <thiagopcosta08@gmail.com>
2026-06-09 19:27:48 -0300

    terminando exercicios de matriz

--- AULA_7/exercicios_matriz/exercicios/exercicio7matriz.cpp
+++ AULA_7/exercicios_matriz/exercicios/exercicio7matriz.cpp
@@ -3,16 +3,22 @@ using namespace std;
 int main(){
     int matrizA[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
     int matrizB[3][3] = {{9, 8, 7}, {6, 5, 4}, {3, 2, 1}};
+    int matrizC[3][3];
     for (int i = 0; i < 3; i++){
         for(int j = 0; j < 3; j++){
-            cout << matrizA[i][j] << " ";
-        }        cout << endl;
-        int multiplicacao = 0;
-        for (int i = 0; i < 3; i++){
-            for(int j = 0; j < 3; j++){
-                multiplicacao += matrizA[i][j] * matrizB[i][j];
+            matrizC[3][3] = 0;
+            for(int k = 0; k < 3; k++){
+                matrizC[i][j] += matrizA[i][k] * matrizB[k][j];
             }
         }
-        cout << "A soma dos elementos da matriz e: " << multiplicacao << endl;
     }
-
+    cout << "Matriz C (resultado da multiplicação de A e B):" << endl;
+    for (int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            cout << matrizC[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+           
